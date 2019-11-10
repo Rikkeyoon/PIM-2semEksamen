a9231f0200d02a1442c68893830bef2d73a35ad8
Rikke Yoon <Rikke@Rikkes-MacBook-Pro-2.local>
2019-11-10 13:00:43 +0100

    format initDB.sql, changed index.jsp

--- src/main/java/persistence/initDB.sql
+++ src/main/java/persistence/initDB.sql
@@ -10,19 +10,19 @@ DROP TABLE IF EXISTS categories;
 DROP TABLE IF EXISTS mobil_category;
 
 CREATE TABLE mobil_category (
-	model VARCHAR(45) NOT NULL,
+    model VARCHAR(45) NOT NULL,
     PRIMARY KEY(model)
 );
 
 CREATE TABLE categories (
-	category_name VARCHAR(45) NOT NULL,
+    category_name VARCHAR(45) NOT NULL,
     category_model VARCHAR(45) NOT NULL,
-	PRIMARY KEY(category_name),
+    PRIMARY KEY(category_name),
     FOREIGN KEY(category_model) REFERENCES mobil_category(model)
 );
 
 CREATE TABLE products (
-	id INT NOT NULL,
+    id INT NOT NULL,
     name VARCHAR(45) NOT NULL,
     description VARCHAR(200) NOT NULL,
     category_name VARCHAR(45) NOT NULL,
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -9,10 +9,14 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Homepage</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
+    
     <body>
         <h1>Pim system</h1>
+        <input type="submit" value="create product" />
+        
     </body>
 </html>
