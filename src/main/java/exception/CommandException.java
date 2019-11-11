9e6a1207625d3639f1942067ca97925d9321b7e3
Caroline Hoeg <cph-ch465rcphbusiness.dk>
2019-11-11 10:50:14 +0100

    Exception folder

---
+++ src/main/java/exception/CommandException.java
@@ -0,0 +1,16 @@
+package exception;
+/**
+ * The purpose of CommandException is to turn Exceptions from the persistence
+ * layer into so-called business exceptions to keep
+ * low coupling between the layers and keep the encapsulation of the persistence
+ * layer
+ *
+ * @author carol
+ */
+public class CommandException extends Exception {
+
+    public CommandException(String msg) {
+        super(msg);
+    }
+
+}
